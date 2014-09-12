9a3df569baebb8b68f3b7f3d24cb2edb77caec1d
Long Jing <long.jing618@gmail.com>
2014-09-12 17:32:13 -0400

    Add TokenFilter - ACCENT

--- edu/buffalo/cse/irf14/analysis/TokenFilter_ACCENT.java
+++ edu/buffalo/cse/irf14/analysis/TokenFilter_ACCENT.java
@@ -1,20 +1,33 @@
 package edu.buffalo.cse.irf14.analysis;
 
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
 public class TokenFilter_ACCENT extends TokenFilter{
 	int length;
 	int count = 0;
 	String currentTokenString;
 	TokenStream copy;
 	Token tempToken;
+	Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
 	public TokenFilter_ACCENT(TokenStream stream) {
 		super(stream);
 		copy = stream;
 		length = copy.tokenList.size();
 	}
 
-	public void increment() throws TokenizerException
+	public boolean increment() throws TokenizerException
 	{
-		
+		if(count < length)
+		{
+			currentTokenString = copy.tokenList.get(count).toString();
+			currentTokenString = Normalizer.normalize(currentTokenString, Normalizer.Form.NFD);
+			currentTokenString = pattern.matcher(currentTokenString).replaceAll("");
+			count++;
+			return true;
+		}
+		else
+			return false;
 	}
 	public TokenStream getStream()
 	{

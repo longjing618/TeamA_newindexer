7b2ffaaafe5968e738c8c62cfb0c63c1c4da25bb
biki23 <kashyap.bikram@gmail.com>
2014-09-19 15:45:22 -0400

    Changed reference, commented main method, and removed unused imports.

--- edu/buffalo/cse/irf14/analysis/Stemmer.java
+++ edu/buffalo/cse/irf14/analysis/Stemmer.java
@@ -2,7 +2,7 @@ package edu.buffalo.cse.irf14.analysis;
 
 /*
  * Taken from http://tartarus.org/martin/PorterStemmer/java.txt
- * Removed main method.
+ * Removed(commented) main method.
  */
 
 /*
@@ -39,7 +39,6 @@ package edu.buffalo.cse.irf14.analysis;
 
 */
 
-import java.io.*;
 
 /**
   * Stemmer, implementing the Porter Stemming Algorithm
@@ -371,62 +370,62 @@ class Stemmer
     * forcing lower case must be done outside the Stemmer class.
     * Usage: Stemmer file-name file-name ...
     */
-   public static void main(String[] args)
-   {
-      char[] w = new char[501];
-      Stemmer s = new Stemmer();
-      for (int i = 0; i < args.length; i++)
-      try
-      {
-         FileInputStream in = new FileInputStream(args[i]);
-
-         try
-         { while(true)
-
-           {  int ch = in.read();
-              if (Character.isLetter((char) ch))
-              {
-                 int j = 0;
-                 while(true)
-                 {  ch = Character.toLowerCase((char) ch);
-                    w[j] = (char) ch;
-                    if (j < 500) j++;
-                    ch = in.read();
-                    if (!Character.isLetter((char) ch))
-                    {
-                       /* to test add(char ch) */
-                       for (int c = 0; c < j; c++) s.add(w[c]);
-
-                       /* or, to test add(char[] w, int j) */
-                       /* s.add(w, j); */
-
-                       s.stem();
-                       {  String u;
-
-                          /* and now, to test toString() : */
-                          u = s.toString();
-
-                          /* to test getResultBuffer(), getResultLength() : */
-                          /* u = new String(s.getResultBuffer(), 0, s.getResultLength()); */
-
-                          System.out.print(u);
-                       }
-                       break;
-                    }
-                 }
-              }
-              if (ch < 0) break;
-              System.out.print((char)ch);
-           }
-         }
-         catch (IOException e)
-         {  System.out.println("error reading " + args[i]);
-            break;
-         }
-      }
-      catch (FileNotFoundException e)
-      {  System.out.println("file " + args[i] + " not found");
-         break;
-      }
-   }
+//   public static void main(String[] args)
+//   {
+//      char[] w = new char[501];
+//      Stemmer s = new Stemmer();
+//      for (int i = 0; i < args.length; i++)
+//      try
+//      {
+//         FileInputStream in = new FileInputStream(args[i]);
+//
+//         try
+//         { while(true)
+//
+//           {  int ch = in.read();
+//              if (Character.isLetter((char) ch))
+//              {
+//                 int j = 0;
+//                 while(true)
+//                 {  ch = Character.toLowerCase((char) ch);
+//                    w[j] = (char) ch;
+//                    if (j < 500) j++;
+//                    ch = in.read();
+//                    if (!Character.isLetter((char) ch))
+//                    {
+//                       /* to test add(char ch) */
+//                       for (int c = 0; c < j; c++) s.add(w[c]);
+//
+//                       /* or, to test add(char[] w, int j) */
+//                       /* s.add(w, j); */
+//
+//                       s.stem();
+//                       {  String u;
+//
+//                          /* and now, to test toString() : */
+//                          u = s.toString();
+//
+//                          /* to test getResultBuffer(), getResultLength() : */
+//                          /* u = new String(s.getResultBuffer(), 0, s.getResultLength()); */
+//
+//                          System.out.print(u);
+//                       }
+//                       break;
+//                    }
+//                 }
+//              }
+//              if (ch < 0) break;
+//              System.out.print((char)ch);
+//           }
+//         }
+//         catch (IOException e)
+//         {  System.out.println("error reading " + args[i]);
+//            break;
+//         }
+//      }
+//      catch (FileNotFoundException e)
+//      {  System.out.println("file " + args[i] + " not found");
+//         break;
+//      }
+//   }
 }
